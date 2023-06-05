e3c8727df2c4fe78d497cdd19fb0b878e71d0e63
maryyj <maria.holster@outlook.com>
2023-06-05 16:33:36 +0200

    add cookies

---
+++ WomensTechForum2.0/Pages/CookiePolicy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WomensTechForum2._0.Pages
+{
+    public class CookiePolicyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
